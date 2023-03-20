af78f40720f0ccd671353e0eea206b2e8ca5eae5
skydragon210 <skydragonsandhippogriffs@gmail.com>
2023-03-20 15:06:24 +0800

    score refactored

---
+++ prac_02/score.py
@@ -0,0 +1,29 @@
+"""
+CP1404 - Practical 2
+Program Name:Score
+Author: Devanandini Chakravarti
+Date:20.03.2023
+"""
+
+
+def main():
+    score = float(input("Enter score: "))
+    result = get_result(score)
+    print(result)
+
+
+def get_result(score):
+    result = ""
+    if score < 0 or 100 < score:
+        result = "Invalid score"
+    else:
+        if score > 90:
+            result = "Excellent"
+        elif score >= 50:
+            result = "Passable"
+        elif score < 50:
+            result = "Bad"
+    return result
+
+main()
+
