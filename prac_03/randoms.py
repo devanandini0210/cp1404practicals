917336c20fc21429141c4ea5eff41b365eb95d6b
skydragon210 <skydragonsandhippogriffs@gmail.com>
2023-03-27 14:02:30 +0800

    randoms.py added

---
+++ prac_03/randoms.py
@@ -0,0 +1,29 @@
+""" What did you see on line 1?
+Answer: 20.
+
+What was the smallest number you could have seen, what was the largest?
+Answer: The smallest number: 5
+The largest number: 20
+
+
+What did you see on line 2?
+Answer: 5
+
+What was the smallest number you could have seen, what was the largest?
+Answer: smallest: 5; largest: 9
+
+Could line 2 have produced a 4?
+Answer:No
+
+
+What did you see on line 3?
+Answer: 3.847160434000594
+
+What was the smallest number you could have seen, what was the largest?
+Answer: smallest: 2.5; largest: 5.5
+
+"""
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+import random
+print(random.randint(1, 100))
