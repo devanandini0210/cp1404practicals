a8ca66361eb41022e62ba9e1dd3bd2c6dcbb9598
skydragon210 <skydragonsandhippogriffs@gmail.com>
2023-04-10 15:49:41 +0800

    word occurrences completed

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,15 @@
+text = input("Text: ")
+text = text.split()
+count = []
+width = 15
+for i in range(len(text)):
+   count.append(0)
+text.sort()
+words_to_occurrences = dict(zip(text, count))
+
+for word in text:
+    if word in words_to_occurrences:
+        words_to_occurrences[word] += 1
+
+for keys, value in words_to_occurrences.items():
+    print(f"{keys:{width}} : {value}")
