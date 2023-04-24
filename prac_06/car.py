268f766ef9ea6b0e8e07e48e983129d91824b786
skydragon210 <skydragonsandhippogriffs@gmail.com>
2023-04-24 14:18:04 +0800

    cars and languages amended

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,14 +4,14 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0, car_name=""):
+    def __init__(self, fuel=0, name=""):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
         self._odometer = 0
-        self.name = car_name
+        self.name = name
 
     def __str__(self):
         return f"{self.name}, fuel{self.fuel}, odometer={self._odometer}"
