88d56c030afd446533e1a6dc822da172854c0532
skydragon210 <skydragonsandhippogriffs@gmail.com>
2023-05-14 00:05:40 +0800

    Completed Miles to KM Conversion Exercise

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -6,7 +6,6 @@ __author__ = 'Devanandini Chakravarti'
 
 
 class MilesConverterApp(App):
-    """ SquareNumberApp is a Kivy App for squaring a number """
 
     def build(self):
         """ build the Kivy app from the kv file """
