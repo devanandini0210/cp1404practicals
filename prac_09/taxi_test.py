9429890bd07b6917df5c5426e56ba8838885a4ec
skydragon210 <skydragonsandhippogriffs@gmail.com>
2023-05-15 13:26:04 +0800

    Completed Taxi Exercise

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,22 @@
+from prac_09.taxi import Taxi
+
+
+def main():
+    my_taxi = Taxi("Prius 1", 100)
+    my_taxi.drive(40)
+    printDetails(my_taxi.name, my_taxi.fuel, my_taxi.price_per_km, my_taxi.get_fare(), my_taxi.current_fare_distance)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    printDetails(my_taxi.name, my_taxi.fuel, my_taxi.price_per_km, my_taxi.get_fare(), my_taxi.current_fare_distance)
+
+
+def printDetails(name, fuel_units, price, fare, current_fare_distance):
+    print(f"Name: {name}")
+    print(f"Fuel Units: {fuel_units}")
+    print(f"Price per km: {price}")
+    print(f"Current Fare Distance: {current_fare_distance}")
+    print(f"Fare: {fare}")
+    print("")
+
+
+main()
