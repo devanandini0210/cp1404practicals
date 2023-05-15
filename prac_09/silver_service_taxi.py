0f493d9f5d9cf77943fc9fdff3ca884a0ac8ff7d
skydragon210 <skydragonsandhippogriffs@gmail.com>
2023-05-15 14:01:55 +0800

    Completed Taxi Exercise

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -6,10 +6,10 @@ class SilverServiceTaxi(Taxi):
 
     def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
-        self.price_per_km = SilverServiceTaxi.price_per_km * fanciness
+        Taxi.price_per_km *= fanciness
 
     def __str__(self):
         return f"{super().__str__()}, plus flagfall of {self.flagfall}"
 
     def get_fare(self):
-        return self.price_per_km * self.current_fare_distance + self.flagfall
+        return super().get_fare()+self.flagfall
